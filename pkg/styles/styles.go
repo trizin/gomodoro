5b68d028e19d652c3b0e54aec082f18bf9672f55
trizin <25263018+trizin@users.noreply.github.com>
2022-05-23 17:29:17 +0300

    Better gradients

--- pkg/styles/styles.go
+++ pkg/styles/styles.go
@@ -5,9 +5,8 @@ import "github.com/charmbracelet/lipgloss"
 var workColor = "#7D56F4"
 var breakColor = "#f02961"
 
-var WorkGradient = [2]string{"#7D56F4", "#f02961"}
-var BreakGradient = [2]string{"#f02961", "#7D56F4"}
-
+var WorkGradient = [2]string{"#7D56F4", "#6638f2"}
+var BreakGradient = [2]string{"#f02961", "#ed0748"}
 
 func GetTextStyle(pomodoroState int) lipgloss.Style {
 	textStyle := lipgloss.NewStyle().
